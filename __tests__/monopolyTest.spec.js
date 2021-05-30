769638585539b55e0b7a09de7f33082abea3a990
n1c0201 <nicarthur21@gmail.com>
2021-05-30 20:53:00 +0700

    finished testings, added board parameter to sell and upgrade function in player.js

--- __tests__/monopolyTest.spec.js
+++ __tests__/monopolyTest.spec.js
@@ -1,5 +1,5 @@
 const propertyList =  require("../src/game/propertydata.js");
-const Player = require("../src/game/Player.js");
+const Player = require("../src/game/player.js");
 const Property = require ('../src/game/property.js');
 const Board = require('../src/game/board.js'); 
 const { TestScheduler } = require("jest");
@@ -7,10 +7,25 @@ const { TestScheduler } = require("jest");
 let board1 = new Board();
 
 board1.initBoard(40);
-
 board1.initGame(4);
+console.log(board1.players[0]);
+board1.players[0].position = 8;
+board1.players[0].buy(board1);
+board1.players[0].upgrade(8, board1);
+board1.players[1].position = 8;
+board1.players[1].checkPosition(board1);
+board1.players[2].position = 1;
+board1.players[2].buy(board1);
+board1.players[2].upgrade(1, board1);
+board1.players[2].sell(1, board1);
+board1.players[3].position = 2;
+board1.players[3].checkPosition(board1);
+console.log(board1.board[8]);
+console.log(board1.board[1]);
+console.log(board1.players);
+
 describe("Moneypoly board test", () => {
-    test("Whether BINUS exists", () => {
-        expect(property).toEqual(expect.anything());
-    })
-})
+   test("Whether BINUS exists", () => {
+        expect(board1.board).toEqual(expect.anything());
+     })
+ })
