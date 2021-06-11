49b261dceaebd94177ed8033f65788f45750abe2
kevinherman1104 <liemkevin1104@gmail.com>
2021-06-11 11:30:13 +0700

    added piece and simple movements

--- public/js/dice.js
+++ public/js/dice.js
@@ -1,10 +1,19 @@
+
+
+// eslint-disable-next-line no-unused-vars
 function rollDice() {
+    let total = 0
     const dice = [...document.querySelectorAll(".die-list")];
     dice.forEach(die => {
       toggleClasses(die);
       die.dataset.roll = getRandomNumber(1, 6);
+      total = total + parseInt(die.dataset.roll)
     });
+    console.log(total)
+    move(piece1, total)
   }
+
+
   
   function toggleClasses(die) {
     die.classList.toggle("odd-roll");
@@ -16,6 +25,19 @@ function rollDice() {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
+ 
   
   document.getElementById("roll-button").addEventListener("click", rollDice);
   
+const piece1 = document.getElementById("piece-1");
+const piece2 = document.getElementById("piece-2");
+const piece3 = document.getElementById("piece-3");
+const piece4 = document.getElementById("piece-4");
+
+function move(piece, randomNumber) {
+  piece.style.transform = `translate(
+      ${randomNumber*50}px,
+      ${0}px)`
+}
+
+
