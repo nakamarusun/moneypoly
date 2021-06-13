5748da77f3c5722ca63a26f3dcf6cba9f03639e6
n1c0201 <nicarthur21@gmail.com>
2021-06-13 23:09:22 +0700

    changed testing

--- __tests__/monopolyTest.spec.js
+++ __tests__/monopolyTest.spec.js
@@ -8,21 +8,26 @@ let board1 = new Board();
 
 board1.initBoard(40);
 board1.initGame(4);
-console.log(board1.players[0]);
-board1.players[0].position = 8;
+// console.log(board1.players);
+board1.players[0].move()
 board1.players[0].buy();
-board1.players[0].upgrade(8);
-board1.players[1].position = 8;
-board1.players[1].checkPosition();
-board1.players[2].position = 1;
-board1.players[2].buy();
-board1.players[2].upgrade(1);
-board1.players[2].sell(1);
-board1.players[3].position = 2;
-board1.players[3].checkPosition();
-console.log(board1.board[8]);
-console.log(board1.board[1]);
+board1.players[0].upgrade();
 console.log(board1.players);
+// board1.players[1].position = 8;
+// board1.players[1].checkPosition();
+// board1.players[2].position = 1;
+// board1.players[2].buy();
+// board1.players[2].upgrade(1);
+// board1.players[2].sell(1);
+// board1.players[3].position = 2;
+// board1.players[3].checkPosition();
+// board1.players[0].status = 2;
+// board1.players[0].move();
+// console.log(board1.players[0].properties[0]);
+// console.log(board1.returnBoard().boardState);
+
+// console.log(board1.board[1]);
+// console.log(board1.players);
 
 describe("Moneypoly board test", () => {
    test("Whether BINUS exists", () => {
