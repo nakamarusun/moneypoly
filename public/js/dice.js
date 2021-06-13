0991ee9326366a23b8d23ece48c793b861be1632
kevinherman1104 <liemkevin1104@gmail.com>
2021-06-13 19:30:09 +0700

    added dialog, managed movements in jail (partial), fixed some button UI

--- public/js/dice.js
+++ public/js/dice.js
@@ -4,7 +4,8 @@ class Piece {
     this.$piece = pieceHTML
     this.boxLength = 96
     this.rectLength = 57
-    this.position = 30
+    this.position = 29
+    this.jail = false
 
     this.xOffset = xOffset
     this.yOffset = yOffset
@@ -18,6 +19,13 @@ class Piece {
     console.log(this.position)
   }
 
+  jailPiece() {
+    this.$piece.style.animation = "toJail 2s both";
+
+  }
+
+ 
+
   move(randomNumber) {
     const num = this.position + randomNumber
     this.position = (num) > 39 ?
@@ -26,10 +34,7 @@ class Piece {
     const coef = Math.floor((this.position)/10)
     const place = this.position % 10
 
-    if (this.position === 30) {
-      // TODO: Kevin just visiting moment
-      return;
-    }
+    
 
     switch (coef) {
       case 0:
@@ -43,20 +48,36 @@ class Piece {
           this.rectLength * 10 + this.xOffset,
           -this.rectLength * place - (place > 0 ? this.yOffset : 0)
         )
+        if (this.position === 10) {
+          this.position = 30;
+          this.jail = true;
+          this.jailPiece();
+        }
         break;
       case 2:
         this.positionPiece(
           (this.rectLength * 10 + this.xOffset) - (this.rectLength * place),
           -this.rectLength * 10 - this.yOffset
         )
+        
         break;
       case 3:
+        
         this.positionPiece(
           0,
           (-this.rectLength * 10 - this.yOffset) + this.rectLength * place
         )
+
+        if (this.position === 30){
+          document.getElementById("piece-1").style.transform = `translate(-7px, -598px)`;
+          document.getElementById("piece-2").style.transform = `translate(-40px, -630px)`;
+          document.getElementById("piece-3").style.transform = `translate(-7px, -594px)`;
+          document.getElementById("piece-4").style.transform = `translate(0px, -660px)`;
+      }
         break;
     }
+    
+    
   }
 
 }
@@ -67,7 +88,7 @@ const piece3 = new Piece(document.getElementById("piece-3"), 34, 24);
 const piece4 = new Piece(document.getElementById("piece-4"), 17, 24);
 
 // eslint-disable-next-line no-unused-vars
-function rollDice() {
+function rollDice(turn) {
   let total = 0
   const dice = [...document.querySelectorAll(".die-list")];
   dice.forEach(die => {
@@ -75,6 +96,18 @@ function rollDice() {
     die.dataset.roll = getRandomNumber(1, 6);
     total = total + parseInt(die.dataset.roll)
   });
+  // if(turn === 1){
+  //   piece1.move(total)
+  // }
+  // else if(turn === 2){
+  //   piece2.move(total)
+  // }
+  // else if(turn === 3){
+  //   piece3.move(total)
+  // }
+  // else if(turn === 4){
+  //   piece4.move(total)
+  // }
   console.log(total)
   piece1.move(1)
   piece2.move(1)
